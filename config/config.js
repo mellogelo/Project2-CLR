5cede4c55c8609df3a610a748e2809e70fbfcab6
Ralph Magbanua <rmagbanua09@gmail.com>
2020-06-11 18:15:56 -0700

    testing out some stuff

--- config/config.js
+++ config/config.js
@@ -6,7 +6,7 @@ module.exports = {
     password: process.env.DB_PASSWORD || "root",
     database: process.env.DB_INSTANCE || "blogger",
     host: process.env.DB_HOST || "127.0.0.1",
-    port: process.env.SERVER_PORT || 3306,
+    port: process.env.SERVER_PORT || 8889,
     dialect: process.env.DB_DIALECT || "mysql",
   },
   test: {
