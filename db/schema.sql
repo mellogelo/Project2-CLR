e5d51baad1f6e6dbc858e3036a4be4252e54296a
Ralph Magbanua <rmagbanua09@gmail.com>
2020-06-13 02:28:39 -0700

    html, css

--- db/schema.sql
+++ db/schema.sql
@@ -3,37 +3,17 @@ DROP DATABASE IF EXISTS forexDB;
 -- Creates the forexDB database
 CREATE DATABASE forexDB;
 -- 
-use forexdb;
+use forexDB;
 --
 
-create table currencys (
-    uuid varchar(36),
-    name varchar(16),
-    code varchar(3),
-    country varchar(255),
-    symbolUnicodeHex varchar(32),
-    isBaseCurrency boolean default false,
-    Primary Key(uuid)
-);
-
-
-create table exchangerates (
-    uuid varchar(36),
-    baseCurrencyCode varchar(3),
-    targetCurrencyCode varchar(5),
-    rate DECIMAL(10,8)
-);
-
-CREATE TABLE accounts (
-    uuid varchar(36),
-    firstName varchar(25),
-    lastName varchar(25),
-    email varchar(30),
-    password varchar(15),
-    baseCurrencyCode varchar(3),
-    initialAmount DECIMAL(10,8)
-);
-insert into accounts (uuid,firstName,lastName,email,password,baseCurrencyCode,initialAmount)
-values
+-- create table currencys (
+--     uuid varchar(36),
+--     name varchar(16),
+--     code varchar(3),
+--     country varchar(255),
+--     symbolUnicodeHex varchar(32),
+--     isBaseCurrency boolean default false,
+--     Primary Key(uuid)
+-- );
 
 -- create unique index idx_currency_code on currencys (code);
